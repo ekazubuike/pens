a540962f936aeddd0f6672261a25c58d880890e0
EK Azubuike <ayo.azubuike@gmail.com>
2017-10-13 23:58:06 -0500

    Add conditional display functionality to disclosure question

--- pens.js
+++ pens.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
   $('.ui.dropdown').dropdown();
-  $('ui.radio.checkbox').checkbox();
+  $('.ui.radio.checkbox').checkbox();
+
+  $('#yesDisclosed').on("click", function(){
+    $('#dateDisclosed').removeClass("conditionalDisplay");
+  });
+  $('#noDisclosed').on("click", function(){
+    $('#dateDisclosed').addClass("conditionalDisplay");
+  });
 });
