cfe81f93364fc1a894120de15c0def09ca92aee6
EK Azubuike <ayo.azubuike@gmail.com>
2017-10-14 23:40:14 -0500

    Add further styling and conditinal functionality to forms

--- pens.js
+++ pens.js
@@ -1,11 +1,33 @@
 $(document).ready(function(){
-  $('.ui.dropdown').dropdown();
-  $('.ui.radio.checkbox').checkbox();
+	//semantic UI JS
+	$('.ui.dropdown').dropdown();
+	$('.ui.radio.checkbox').checkbox();
 
-  $('#yesDisclosed').on("click", function(){
-    $('#dateDisclosed').removeClass("conditionalDisplay");
-  });
-  $('#noDisclosed').on("click", function(){
-    $('#dateDisclosed').addClass("conditionalDisplay");
-  });
+	//display disclosure date input if "yes"
+	$('#yesDisclosed').on("click", function(){
+		$('#dateDisclosed').removeClass("conditionalDisplay");
+    });
+
+	//hide disclosure date if "no"
+    $('#noDisclosed').on("click", function(){
+    	$('#dateDisclosed').addClass("conditionalDisplay");
+    });
+
+    //show infection description if checkbox checked
+    $("#otherInfection").change(function() {
+    	if(this.checked){
+    		$("#infectionDescription").removeClass("conditionalDisplay");
+    	} else {
+    		$("#infectionDescription").addClass("conditionalDisplay");
+    	}
+    });
+
+    //display text box if other reason
+	$("#otherTest").change(function(){
+		if(this.checked){
+			$("#otherTestReason").removeClass("conditionalDisplay");
+		} else if (!this.checked){
+			$("#otherTestReason").addClass("conditionalDisplay");
+		}
+	});
 });
